c8f6db967e43273fcd8f8dc8b561edd9848cb814
broonzy <ger_daly@yahoo.com>
2014-10-04 21:14:09 +0100

    optimised all images

--- js/hover.js
+++ js/hover.js
@@ -1,5 +1,5 @@
 
- $(".window_elearning").hover(
- function() { $(this).children(".elearning_link").fadeIn(000) },
- function() { $(this).children(".elearning_link").fadeOut(000); }
+ $(".window").hover(
+ function() { $(this).children(".window_link").fadeIn(000) },
+ function() { $(this).children(".window_link").fadeOut(000); }
 );
